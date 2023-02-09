cb21ddb97f02033b13b7b9d18215c32267dce311
juan.esquivelm <juan.esquivelm@yucatan.gob.mx>
2023-02-09 16:32:43 -0600

    Agregada la vista y la acción para mostrar los resultados de la exportación

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -10,6 +10,13 @@ class TablesController < ApplicationController
         row = Hash[[header, fileInfo.row(i)].transpose]
         Table.create!(cliente: row["Cliente"], descripcion: row["Descripción del Producto"], precio: row["Precio por pieza"], cantidad: row["Numero de piezas"], direccion_vendedor: row["Diección del vendedor"], vendedor: row["Nombre del Vendedor"])
         end
-        redirect_to create_table_path, notice: "Tabla creada exitósamente."
+        redirect_to action: "show"
+    end
+    def show
+        @total = 0
+        @data = Table.all
+        @data.each do |sale|
+            @total+= sale.precio*sale.cantidad
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/addtable', to: 'tables#index'
   post '/addtable', to: 'tables#save', as: 'create_table'
+  get '/show', to: 'tables#show', as: 'show_result'
 
   # Defines the root path route ("/")
   # root "articles#index"
